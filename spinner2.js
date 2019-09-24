a1c4dc0b21df8ab7f5721dea13b644d4775bee37
caitlining <caitlincing@gmail.com>
2019-09-24 20:42:18 +0000

    Submitting code for Spinner II assignment, completed with Pair Programming with @Justin-Lighthouse

---
+++ spinner2.js
@@ -0,0 +1,13 @@
+const spinners = ['|', '/', '-', '\\'];
+let delay = spinners.length * 10
+
+for (let i = 0; i < delay; i++) {
+  setTimeout(() => {
+    process.stdout.write(`\r${spinners[i % spinners.length]}`);
+    if (i === delay - 1) {
+      process.stdout.write('\n');
+    }
+  }, i * 100);
+};
+
+
